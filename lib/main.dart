bfa8ae6557bb5b0274b22c1969b978ecca30b8c2
Souza83 <tec.psouza@gmail.com>
2022-03-01 08:57:14 -0300

    Criando a primeira arvore de widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,16 @@ main() => runApp(PerguntaApp());
 
 //Cria classe (extends: demonstra que há herança)
 class PerguntaApp extends StatelessWidget {
+  @override //Decorador que demonstra ao StatelessWidget é obrigado implementar
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Olá Flutteiros!'),
+      home: Scaffold(
+        //Inicia a estrutura da aplicação
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá Flutter!!!'),
+      ),
     );
   }
 }
